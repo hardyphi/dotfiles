f6b65a690df13ae2883467d367488da3e1fab28e
Phil Hardy <pehardy12@gmail.com>
2022-09-09 22:51:47 +0100

    dtf alias for fish and bash

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias dtf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+set PATH /home/philip/.nvm/versions/node/v16.15.0/bin $PATH
