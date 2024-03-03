6e8968a18676dccc4200caef111d67628f6c9453
Phil Hardy <pehardy12@gmail.com>
2024-03-03 17:23:30 +0000

    matching some aliases between nvim and fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+bind \cy forward-word
+bind \ey forward-char
+
 alias vi=nvim
 
 set PATH /home/philip/.nvm/versions/node/v16.15.0/bin $PATH
