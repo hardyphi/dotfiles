20c37bfb3e34dc09d99ea057d71dba4c56936833
Phil Hardy <pehardy12@gmail.com>
2024-03-03 16:20:05 +0000

    removing bare-repo alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias dtf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias dft='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
 alias vi=nvim
 
 set PATH /home/philip/.nvm/versions/node/v16.15.0/bin $PATH
